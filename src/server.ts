0a186f298be09743729287b15082bcb17d30966b
AllefRamos14 <mariaalice6123@gmail.com>
2026-04-05 18:30:12 -0300

    fix: ajustando porta e host para deploy no render

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,8 @@ import app from "./app";
 import { prismaConnect } from "./config/prisma";
 import { env } from "./config/env";
 import initializeFirebaseAdmin from "./config/firebase";
-const PORT = env.PORT;
 
-
+const PORT = Number(process.env.PORT) || env.PORT || 3001;
 
 initializeFirebaseAdmin();
 
@@ -12,14 +11,16 @@ const startServer = async () => {
     try {
         await prismaConnect();
 
-        await app.listen({ port: PORT }).then(() => {
-            console.log(`Servidor Rodando Na Port ${PORT}`)
-        })
+        await app.listen({
+            port: PORT,
+            host: "0.0.0.0",
+        });
+
+        console.log(`Servidor Rodando Na Port ${PORT}`);
     } catch (error) {
-
-        console.log(error)
+        console.log(error);
+        process.exit(1);
     }
-}
-startServer()
+};
 
-
+startServer();
