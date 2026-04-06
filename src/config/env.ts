561d336eec2e2991e83a0d24ff6dd4515006c033
AllefRamos14 <mariaalice6123@gmail.com>
2026-04-06 20:03:37 -0300

    fix: ajustar validacao das variaveis de ambiente

--- src/config/env.ts
+++ src/config/env.ts
@@ -1,27 +1,53 @@
+// import { z } from "zod";
+// import dotenv from "dotenv";
+
+
+// dotenv.config();
+
+// const envSchema = z.object({
+//     PORT: z.string().transform(Number).default(3001),
+//     DATABASE_URL: z.string().min(5, "DATABASE_URL é obrigatório"),
+//     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
+
+//     // firebase
+//     FIREBASE_PROJECT_ID: z.string().optional(),
+//     FIREBASE_PRIVATE_KEY: z.string().optional(),
+//     FIREBASE_CLIENT_EMAIL: z.string().optional(),
+
+// });
+
+// const _env = envSchema.safeParse(process.env);
+
+// if (!_env.success) {
+//     console.error("Variaveis de ambiente INVÁLIDAS");
+//     process.exit(1);
+
+// }
+
+// export const env = _env.data;
+
+
 import { z } from "zod";
 import dotenv from "dotenv";
-
 
 dotenv.config();
 
 const envSchema = z.object({
-    PORT: z.string().transform(Number).default(3001),
+    PORT: z.coerce.number().default(3001),
     DATABASE_URL: z.string().min(5, "DATABASE_URL é obrigatório"),
     NODE_ENV: z.enum(["development", "test", "production"]).default("development"),
 
-    // firebase
-    FIREBASE_PROJECT_ID: z.string().optional(),
-    FIREBASE_PRIVATE_KEY: z.string().optional(),
-    FIREBASE_CLIENT_EMAIL: z.string().optional(),
-
+    FIREBASE_PROJECT_ID: z.string().min(1, "FIREBASE_PROJECT_ID é obrigatório"),
+    FIREBASE_PRIVATE_KEY: z.string().min(1, "FIREBASE_PRIVATE_KEY é obrigatório"),
+    FIREBASE_CLIENT_EMAIL: z.string().min(1, "FIREBASE_CLIENT_EMAIL é obrigatório"),
 });
 
 const _env = envSchema.safeParse(process.env);
 
 if (!_env.success) {
     console.error("Variaveis de ambiente INVÁLIDAS");
+    console.error(_env.error.format());
     process.exit(1);
-
 }
 
 export const env = _env.data;
